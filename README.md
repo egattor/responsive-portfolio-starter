d7af322afd0f99efa0a2875a02d524b16318241b
Ernest Apedo <egattorr@gmail.com>
2024-07-30 13:54:15 +0000

    Give this repo a STAR!

---
+++ README.md
@@ -0,0 +1,5 @@
+### [Watch Tutorial](https://youtu.be/ekAbxVNcy8Q)
+## [Complete Project Source Code](https://buymeacoffee.com/egator/e/283325
+)
+
+![](./thumbnail.jpg)
---
+++ index.js
@@ -0,0 +1,107 @@
+const experienceContent = `
+            <h4>Experience</h4>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque a, architecto laboriosam ab eaque et.
+                </p>
+                <ul>
+                    <li>
+                        <h6>2018 - 2019</h6>
+                        <h5>Intern</h5>
+                        <p>Mobile Telecom Company</p>
+                    </li>
+                    <li>
+                        <h6>2019 - 2021</h6>
+                        <h5>Freelance Web Design</h5>
+                        <p>Fiverr and Upwork</p>
+                    </li>
+                    <li>
+                        <h6>2021 - 2022</h6>
+                        <h5>Frontend Developer</h5>
+                        <p>MTN Ghana</p>
+                    </li>
+                    <li>
+                        <h6>2022 - 2024</h6>
+                        <h5>Fullstack Freelancer</h5>
+                        <p>Self Employed</p>
+                    </li>
+                </ul>
+`
+
+
+
+const educationContent = `<h4>Education</h4>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores.</p>
+                <ul>
+                    <li>
+                        <h5>College Education</h5>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi voluptas maiores qui doloremque accusantium atque vero facilis.
+                        </p>
+                    </li>
+                    <li>
+                        <h5>Frontend Education</h5>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi voluptas maiores qui doloremque accusantium atque vero facilis.
+                        </p>
+                    </li>
+                    <li>
+                        <h5>Backend Education</h5>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi voluptas maiores qui doloremque accusantium atque vero facilis.
+                        </p>
+                    </li>
+                </ul>`
+
+
+
+
+                const skillsContent = `<h4>Skills</h4>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+                <ul>
+                    <li><img src="./assets/react.webp" alt="ReactJS logo"></li>
+                    <li><img src="./assets/next.png" alt="NextJS logo"></li>
+                    <li><img src="./assets/tailwind.png" alt="Tailwind logo"></li>
+                    <li><img src="./assets/prisma.png" alt="Prisma logo"></li>
+                    <li><img src="./assets/mongo.jpg" alt="MongoDB logo"></li>
+                    <li><img src="./assets/jwt.png" alt="JWT logo"></li>
+                    <li><img src="./assets/node.png" alt="NodeJS logo"></li>
+                </ul>`
+
+
+
+`<h4>About me</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit debitis excepturi quibusdam.</p>
+                <ul>
+                    <li>
+                        <h6>Name:</h6>
+                        <h5>Ernest Achiever</h5>
+                    </li>
+                    <li>
+                        <h6>Experience:</h6>
+                        <h5>6+ years</h5>
+                    </li>
+                    <li>
+                        <h6>Email:</h6>
+                        <h5>youremail@gmail.com</h5>
+                    </li>
+                    <li>
+                        <h6>Nationality:</h6>
+                        <h5>Ghanaian</h5>
+                    </li>
+                    <li>
+                        <h6>Freelance & collabs:</h6>
+                        <h5>Available</h5>
+                    </li>
+                    <li>
+                        <h6>Language:</h6>
+                        <h5>English</h5>
+                    </li>
+                    <li>
+                        <h6>Phone:</h6>
+                        <h5>+233557097546</h5>
+                    </li>
+                    <li>
+                        <h6>LinkedIn:</h6>
+                        <h5>@yourhandle</h5>
+                    </li>
+                </ul>`
